0cd15acd2e4c33185c2188b5b99e95ffbaa88be2
Murilo Duarte <murilo.duarte@actminds.com>
2018-04-25 17:00:27 -0300

    added standard to packages, modified readme.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # resume
 
 my personal resume.
+
+## environment
+
+install the following packages.
+
+`npm i -g @angular/cli`
+`npm i -g typescript`
+`npm i -g tslint`
--- src/app/shared/functions/http-loader-factory.ts
+++ src/app/shared/functions/http-loader-factory.ts
@@ -1,6 +1,6 @@
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { HttpClient } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+import { HttpClient } from '@angular/common/http'
 
-export function HttpLoaderFactory(http: HttpClient) {
-    return new TranslateHttpLoader(http);
+export function HttpLoaderFactory (http: HttpClient) {
+    return new TranslateHttpLoader(http)
 }
