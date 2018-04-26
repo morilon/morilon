02a053aa32811b509a63684aec76d6f5f683bc5f
Murilo Duarte <murilo.duarte@actminds.com>
2018-04-26 14:22:50 -0300

    certifications ok

---
+++ src/app/core/models/certifications.ts
@@ -0,0 +1,14 @@
+import { Exam } from '@models/exam'
+
+export class Certification {
+    title: string
+    certsTitle: string
+    examsTitle: string
+    exams: Exam[]
+    courses: string[]
+
+    constructor() {
+        this.exams = []
+        this.courses = []
+    }
+}
--- src/app/sections/certifications/certifications.component.ts
+++ src/app/sections/certifications/certifications.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { Certification } from '@models/certifications'
+import { LanguageService } from '@services/language.service'
+import { Exam } from '@models/exam'
 
 @Component({
   selector: 'app-certifications',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./certifications.component.css']
 })
 export class CertificationsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  certification: Certification
+  private keys = {
+    title: 'Certifications.Title',
+    certsTitle: 'Certifications.CertsTitle',
+    examsTitle: 'Certifications.ExamsTitle'
   }
 
+  constructor(private langService: LanguageService) { }
+
+  ngOnInit() {
+    this.certification = {
+      exams: [
+        Exam.create('http://www.mycertprofile.com/Profile/8786735631/86/2690', 'ms.png', 'MCSA - Microsoft® Certified Solutions Associate: Web Applications', 'G344-8436'),
+        Exam.create('http://www.mycertprofile.com/Profile/8786735631/12/1519', 'ms.png', 'MCP - Microsoft® Certified Professional', 'F599-4871')
+      ],
+      courses: [
+        'ASP.NET Core + Angular 2',
+        'ASP.NET MVC 5 - Enterprise Applications',
+        'edX Azure Skills Toolkit'
+      ]
+    } as Certification
+    this.translate()
+    this.langService.onReload.subscribe(this.translate.bind(this))
+  }
+
+  translate(): void {
+    this.langService.translate.get([this.keys.title, this.keys.certsTitle, this.keys.examsTitle])
+      .subscribe(val => {
+        this.certification.title = val[this.keys.title]
+        this.certification.certsTitle = val[this.keys.certsTitle]
+        this.certification.examsTitle = val[this.keys.examsTitle]
+      })
+  }
 }
