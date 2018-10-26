233ed5bba86441a070543480a7c256a763429d8c
Murilo Duarte <murilo.duarte@actminds.com>
2018-10-26 16:14:39 -0300

    updated environment

--- src/app/core/services/environment.service.ts
+++ src/app/core/services/environment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core'
-import { environment } from '@env/environment'
+import { environment } from 'environments/environment'
 import { Environment } from '@models/environment'
 
 @Injectable()
